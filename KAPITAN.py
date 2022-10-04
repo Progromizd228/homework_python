b27dd0aecfea6c194afa9bed63a1460d1729b002
Progromizd228 <113104975+Progromizd228@users.noreply.github.com>
2022-10-04 11:29:28 +0300

    Delete KAPITAN.py

--- KAPITAN.py
+++
@@ -1,7 +0,0 @@
-from datetime import date
-def captain_log(c_year, c_month, c_day, c_entry):
-    log_date = date(c_year, c_month, c_day)
-    log_entry = open('CaptainLog.txt', 'a')
-    log_entry.write("%s: %s\n" % (log_date, c_entry))
-    log_entry.close()
-captain_log(1912, 4, 15, 'LET\'S RAM THAT ICEBERG!!!')
