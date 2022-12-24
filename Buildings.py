23e038e59008f423dae6a536a9913556cd0c667e
Progromizd228 <113104975+Progromizd228@users.noreply.github.com>
2022-12-24 11:24:39 +0300

    Update Buildings.py

--- Buildings.py
+++ Buildings.py
@@ -1,3 +1,5 @@
+import csv
+
 class InvalidBuilding(Exception):
     pass
 
@@ -19,19 +21,22 @@ class Building:
 
 opt = input("Что вы хотите сделать?\n1. Прочитать данные из файла\n2. Записать данные в файл\n")
 if int(opt) == 1:
-    f = open("Building.txt", "r")
-    print(f.read())
+    with open('Building', 'rt') as f:
+        csv_reader = csv.reader(f)
+        for line in csv_reader:
+            print('Name: {0}\nStoreys: {1}\nHeight: {2}\nWidth: {3}\n'.format(line[3], line[0], line[1], line[2]))
 elif int(opt) == 2:
     x = input("Введите кол-во этажей: ")
     y = input("\nВведите высоту здания: ")
     z = input("\nВведите ширину здания: ")
     n = input("\nВведите название здания: ")
     try:
-        if all(isinstance(vars, (float, int)) for vars in [int(x), int(y), int(z)]):
-            building = Building(int(x), int(y), int(z), n)
-            f = open("Building.txt", "a")
-            f.write("Ваше здание:" + str(building.show()) + "\n")
+        building = Building(int(x), int(y), int(z), n)
     except ValueError:
         print('Нужно вводить число, зачтите задание пж')
+    else:
+        with open("Building", "a") as f:
+            csv_writer = csv.writer(f)
+            csv_writer.writerow([building.storey, building.height, building.width, building.name])
 else:
     raise Exception("Пожалуйста, выберите 1 или 2 вариант.\n")
